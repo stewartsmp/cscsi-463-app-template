55922c5602d625313022c6bf85e659401b27c906
Stewart Million-Perez <stewart.millionperez@und.edu>
2022-02-09 17:47:14 -0700

    database Create change

---
+++ databaseCreation.py
@@ -0,0 +1,12 @@
+"""
+Created on Sat Jan 29 19:20:02 2022
+
+CSCI 463 - Team Project Spring 2022 - Bug SMASHER 
+1/29/2021 - Start, initial skeleton
+
+@author: Stewart
+"""
+
+# bugSMASHER page 2
+
+print ("hello world")
